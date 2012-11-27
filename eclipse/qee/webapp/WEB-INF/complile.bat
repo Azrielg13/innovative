50a105267eccbe88869820c9b3a1e5c6b59c1859
Eddie Mayfield <eddiemay1999@yahoo.com>
2012-11-27 06:22:07 -0800

    delete eclipse

--- eclipse/qee/webapp/WEB-INF/complile.bat
+++
@@ -1,4 +0,0 @@
-cd classes
-for %%j in (com/digitald4/order/*.java) do javac -d ../../WEB-INF/classes/ com/digitald4/order/%%j
-for %%j in (com/digitald4/order/servlet/*.java) do javac -d ../../WEB-INF/classes/ com/digitald4/order/servlet/%%j
-pause
