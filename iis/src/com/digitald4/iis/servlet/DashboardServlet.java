cc375ba906044e735e9e736f58b3aa8380d5ee9d
Eddie Mayfield <eddiemay1999@yahoo.com>
2013-06-24 20:51:23 -0700

    Added global calendar to dashboard

--- iis/src/com/digitald4/iis/servlet/DashboardServlet.java
+++ iis/src/com/digitald4/iis/servlet/DashboardServlet.java
@@ -1,10 +1,13 @@
 package com.digitald4.iis.servlet;
 
+import java.util.Calendar;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.digitald4.common.servlet.ParentServlet;
+import com.digitald4.iis.model.Appointment;
 
 public class DashboardServlet extends ParentServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException {
@@ -13,6 +16,7 @@ public class DashboardServlet extends ParentServlet {
 			PendingAssServlet.setupTable(request);
 			PendingIntakeServlet.setupTable(request);
 			PendingPaymentServlet.setupTable(request);
+			setupCalendar(request);
 			getLayoutPage(request, "/WEB-INF/jsp/dashboard.jsp").forward(request, response);
 		} catch(Exception e) {
 			throw new ServletException(e);
@@ -21,5 +25,11 @@ public class DashboardServlet extends ParentServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException {
 		doGet(request,response);
+	}
+	
+	public static void setupCalendar(HttpServletRequest request) {
+		request.setAttribute("year", Calendar.getInstance().get(Calendar.YEAR));
+		request.setAttribute("month", Calendar.getInstance().get(Calendar.MONTH) + 1);
+		request.setAttribute("appointments", Appointment.getAllActive());
 	}
 }
--- iis/webapps/iis/WEB-INF/jsp/dashboard.jsp
+++ iis/webapps/iis/WEB-INF/jsp/dashboard.jsp
@@ -2,9 +2,19 @@
 <%@ page import="java.util.Collection"%>
 <%@ page import="com.digitald4.common.dao.*"%>
 <%@ page import="com.digitald4.common.component.Column"%>
-
+<%@ page import="com.digitald4.iis.model.*"%>
+<%
+	int year = (Integer)request.getAttribute("year");
+	int month = (Integer)request.getAttribute("month");
+	Collection<Appointment> appointments = Appointment.getAllActive();
+%>
 <article class="container_8">
 	<dd4:table title="Pending Intakes" columns="<%=(Collection<Column>)request.getAttribute(\"pintake_cols\")%>" data="<%=(Collection<? extends DataAccessObject>)request.getAttribute(\"patients\")%>"/>
 	<dd4:table title="Pending Assessment" columns="<%=(Collection<Column>)request.getAttribute(\"penass_cols\")%>" data="<%=(Collection<? extends DataAccessObject>)request.getAttribute(\"appointments\")%>"/>
 	<dd4:table title="Pending Assessment" columns="<%=(Collection<Column>)request.getAttribute(\"payable_cols\")%>" data="<%=(Collection<? extends DataAccessObject>)request.getAttribute(\"payables\")%>"/>
+</article>
+<article class="container_12">
+	<div id="cal_sec" class="grid_8">
+		<dd4:largecal title="Monthly Calendar" userId="<%=0%>" year="<%=year%>" month="<%=month%>" events="<%=appointments%>"/>
+	</div>
 </article>
