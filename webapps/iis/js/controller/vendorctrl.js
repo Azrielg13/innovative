9f4418b8b20055f2a8162948fff54c699fd37acf
Eddie Mayfield <eddiemay1999@yahoo.com>
2016-08-03 11:59:39 -0700

    More 2.0 Updates

--- webapps/iis/js/controller/vendorctrl.js
+++ webapps/iis/js/controller/vendorctrl.js
@@ -3,17 +3,36 @@ com.digitald4.iis.VendorCtrl = function($routeParams, $filter, restService) {
 	this.vendorId = parseInt(vendorId, 10);
 	this.TableType = {
 		PATIENTS: {base: com.digitald4.iis.TableBaseMeta.PATIENTS,
-			request: {query_param: [{column: 'billing_id', operan: '=', value: vendorId]},
-		PENDING_ASSESSMENT: {base: com.digitald4.iis.TableBaseMeta.PATIENTS,
+			request: {query_param: [{column: 'billing_id', operan: '=', value: vendorId}]}},
+		PENDING_ASSESSMENT: {base: com.digitald4.iis.TableBaseMeta.PENDING_ASSESSMENT,
 			request: {query_param: [{column: 'billing_vendor_id', operan: '=', value: vendorId},
 			                        {column: 'assessment_complete', operan: '=', value: 'false'},
-			                        {column: 'start', operan: '<', value: Date.now().toString()}]},
+			                        {column: 'start', operan: '<', value: Date.now().toString()}]}},
 		UNPAID_INVOICES: {base: com.digitald4.iis.TableBaseMeta.INVOICES,
 			request: {query_param: [{column: 'vendor_id', operan: '=', value: vendorId},
-			                        {column: 'status_id', operan: '=', value: '1521'}]},
+			                        {column: 'status_id', operan: '=', value: '1521'}]}},
 		PAID_INVOICES: {base: com.digitald4.iis.TableBaseMeta.INVOICES,
 			request: {query_param: [{column: 'vendor_id', operan: '=', value: vendorId},
-			                        {column: 'status_id', operan: '=', value: '1520'}]},
+			                        {column: 'status_id', operan: '=', value: '1520'}]}},
+		BILLABlE: {base: {title: 'Billable',
+				url: 'appointments',
+				link: {url: '#appointment', title: 'Patient'},
+				columns: [{title: 'Date', getValue: function(appointment) {
+										return $filter('date')(appointment.start, 'MM/dd/yyyy HH:mm');}},
+				          {title: 'Billing Type', prop: 'billing_type_id'},
+				          {title: 'Billed Hours', prop: 'billed_hours'},
+				          {title: 'Hourly Rate', prop: 'billing_rate'},
+				          {title: 'Per Visit Cost', prop: 'billing_flat'},
+				          {title: 'Billed Mileage', prop: 'billing_mileage'},
+				          {title: 'Mileage Rate', prop: 'billing_mileage_rate'},
+				          {title: 'Total Payment', getValue: function(appointment) {
+				          	return $filter('currency')(appointment.billing_flat +
+				          			appointment.billed_hours * appointment.billing_rate +
+				          			appointment.billing_mileage * appointment.billing_mileage_rate);
+				          }}]},
+      request: {query_param: [{column: 'billing_vendor_id', operan: '=', value: vendorId},
+			                        {column: 'assessment_complete', operan: '=', value: 'true'},
+			                        {column: 'invoice_id', operan: ' IS ', value: 'null'}]}},
 	};
 	this.vendorService = new com.digitald4.iis.ProtoService('vendor', restService);
 	this.selectedTab = this.TABS.general;
