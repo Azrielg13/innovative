ed97f2fda4c914c094bfc1b5950a01876340e615
eddiemay <eddiemay1999@yahoo.com>
2021-10-26 21:58:39 -0700

    Fixing of vendor page and js syntax error in vendoraddctrl.js

--- src/main/js/vendoraddctrl.js
+++ src/main/js/vendoraddctrl.js
@@ -1,12 +1,12 @@
 com.digitald4.iis.VendorAddCtrl = function(vendorService) {
   this.vendorService = vendorService;
-  this.vendor = {address = {}};
+  this.vendor = {address: {}};
 };
 
 com.digitald4.iis.VendorAddCtrl.prototype.create = function() {
   this.vendorService.create(this.vendor, function(vendor) {
     this.lastAdded = vendor;
     this.message = 'Vendor added';
-    this.vendor = {address = {}};
+    this.vendor = {address: {}};
   }.bind(this), notify);
 };
